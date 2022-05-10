11c0fe160b7e74f9a90b0a4753e1589e34579851
user <liberaboit@hotmail.com>
2022-05-10 21:21:29 +0900

    0510

---
+++ run0.sh
@@ -0,0 +1,3 @@
+PYTHONHASHSEED=1 python main_vw.py --node keti_3080 --fusion rmc5 --gpu 0 --aggregation-function bilinear --folder-name rmc5_bilinear_vw
+
+PYTHONHASHSEED=1 python main_vw.py --node keti_3080 --fusion rmc5 --gpu 0 --aggregation-function gap --folder-name rmc5_gap_vw
---
+++ run1.sh
@@ -0,0 +1,3 @@
+PYTHONHASHSEED=1 python main_vw.py --node keti_3080 --fusion r2d --gpu 1 --aggregation-function bilinear --folder-name r2d_bilinear_vw
+
+PYTHONHASHSEED=1 python main_vw.py --node keti_3080 --fusion r2d --gpu 1 --aggregation-function gap --folder-name r2d_gap_vw
